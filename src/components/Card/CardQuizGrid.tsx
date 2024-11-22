463b304979f6f5c2894eb4749c7161080ddc83d4
Aslam <aslam>
2024-11-22 20:30:07 +0530

    Update

--- src/components/Card/CardQuizGrid.tsx
+++ src/components/Card/CardQuizGrid.tsx
@@ -1,16 +1,7 @@
-import {
-  IconCalendarDue,
-  IconMessageDots,
-  IconUserCircle,
-} from "@tabler/icons-react";
-import Image, { StaticImageData } from "next/image";
+import { IconUserCircle } from "@tabler/icons-react";
 import Link from "next/link";
-import H3 from "../sharedComponents/H3";
 import H4 from "../sharedComponents/H4";
-import TextL from "../sharedComponents/TextL";
 import TextM from "../sharedComponents/TextM";
-import VectorCardRightBottom from "../vector/VectorCardRightBottom";
-import TextXS from "../sharedComponents/TextXS";
 import TextS from "../sharedComponents/TextS";
 
 //types
@@ -22,33 +13,15 @@ type QuizeType = {
   userName: string
 }
 
-const CardQuizGrid = ({ title, description, createdAt, userName }: QuizeType) => {
+const CardQuizGrid = ({ title, description, userName }: QuizeType) => {
   return (
     <div className="flex max-w-[400px] flex-col items-center gap-6 self-stretch">
-      {/* <div className="relative ">
-        <Image
-          src={"/images/homeOne/explore-courses-1.png"}
-          width={400}
-          height={306}
-          alt="Card Image"
-          className="rounded-32px"
-        />
-        <VectorCardRightBottom />
-      </div> */}
       <div>
         <div className="flex flex-col gap-3 ">
           <div className="flex justify-between max-sm:flex-col max-sm:gap-3 sm:items-center">
             <div className="flex items-center gap-1 max-sm:justify-between">
               <IconUserCircle size={18} /> <TextS>By {userName}</TextS>
             </div>
-            {/* <div className="hidden h-1 w-1 rounded-full bg-neutral-700 sm:block"></div> */}
-            {/* <div className="flex items-center gap-2 max-sm:justify-between">
-              <IconCalendarDue size={28} /> <TextL>{createdAt}</TextL>
-            </div> */}
-            {/* <div className="hidden h-1 w-1 rounded-full bg-neutral-700 sm:block"></div> */}
-            {/* <div className="flex items-center gap-2 max-sm:justify-between">
-              <IconMessageDots size={28} /> <TextL>kkk</TextL>
-            </div> */}
           </div>
           <H4>
             <Link href="/">{title}</Link>
