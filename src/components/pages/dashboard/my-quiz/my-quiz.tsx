5e71098c3013bfdc74b1823f56b876b9e8be3b7a
Aslam <aslam>
2024-11-24 18:49:12 +0530

    Update

--- src/components/pages/dashboard/my-quiz/my-quiz.tsx
+++ src/components/pages/dashboard/my-quiz/my-quiz.tsx
@@ -1,20 +1,23 @@
 "use client";
-import { Accordion, AccordionDetails, AccordionSummary, Autocomplete, TextField } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Autocomplete, Button, TextField } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from 'react';
+import { questionObj } from './MyQuizSchema';
 
 
 type QuestionType = {
     question: string,
     answer: Array<string>
-    correctAnswer: number
+    correctAnswer: number | null
 }
+
+
 const MyQuiz = () => {
-    const [questions, setQuestions] = useState<Array<QuestionType>>([]);
+    const [questions, setQuestions] = useState<Array<QuestionType>>([questionObj]);
+    const [expanded, setExpanded] = useState<number | null>(null);
     return (
         <>
-
             <Grid container spacing={1}>
                 <Grid size={3}>
                     <TextField label="Title" variant="standard" fullWidth />
@@ -48,46 +51,60 @@ const MyQuiz = () => {
                 </Grid>
             </Grid>
             <br />
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1-content"
-                    id="panel1-header"
-                >
-                    Quesion 1
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Grid container spacing={2}>
-                        <Grid size={12}>
-                            <TextField label="Quesion" variant="standard" fullWidth />
-                        </Grid>
-                        <Grid size={6}>
-                            <TextField label="Answer 1" variant="standard" fullWidth />
-                        </Grid>
-                        <Grid size={6}>
-                            <TextField label="Answer 2" variant="standard" fullWidth />
-                        </Grid>
-                        <Grid size={6}>
-                            <TextField label="Answer 3" variant="standard" fullWidth />
-                        </Grid>
-                        <Grid size={6}>
-                            <TextField label="Answer 4" variant="standard" fullWidth />
-                        </Grid>
-                        <Grid size={6}>
-                            <Autocomplete
-                                options={[]}
-                                renderInput={(params) =>
-                                    <TextField
-                                        {...params}
-                                        label="Correct Answer"
-                                        variant="standard"
-                                    />
-                                }
-                            />
-                        </Grid>
-                    </Grid>
-                </AccordionDetails>
-            </Accordion>
+            <Grid container spacing={1} justifyContent="flex-end"
+                alignItems="flex-end">
+                <Grid size={12}>
+                    {questions.map((question, index) => (
+                        <Accordion
+                            key={index}
+                            expanded={expanded === index}
+                            onChange={(_, isExpanded) => setExpanded(isExpanded ? index : null)}
+                        >
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                            >
+                                {index !== expanded && question.question}
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Grid container spacing={2}>
+                                    <Grid size={12}>
+                                        <TextField value={question.question} label="Quesion" variant="standard" fullWidth />
+                                    </Grid>
+                                    <Grid size={6}>
+                                        <TextField value={question.answer[0]} label="Answer 1" variant="standard" fullWidth />
+                                    </Grid>
+                                    <Grid size={6}>
+                                        <TextField value={question.answer[1]} label="Answer 2" variant="standard" fullWidth />
+                                    </Grid>
+                                    <Grid size={6}>
+                                        <TextField value={question.answer[2]} label="Answer 3" variant="standard" fullWidth />
+                                    </Grid>
+                                    <Grid size={6}>
+                                        <TextField value={question.answer[3]} label="Answer 4" variant="standard" fullWidth />
+                                    </Grid>
+                                    <Grid size={6}>
+                                        <Autocomplete
+                                            options={[]}
+                                            renderInput={(params) =>
+                                                <TextField
+                                                    {...params}
+                                                    label="Correct Answer"
+                                                    variant="standard"
+                                                />
+                                            }
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </AccordionDetails>
+                        </Accordion>
+                    ))}
+                </Grid>
+                <Button variant="contained" onClick={() => setQuestions([...questions, questionObj])}>
+                    Add Question
+                </Button>
+            </Grid>
         </>
     );
 };
