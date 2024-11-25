5d6537ca36c67703905939f58315e9085043abe8
Aslam <aslam>
2024-11-25 21:17:03 +0530

    Update

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,9 +2,11 @@ import axios from 'axios';
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL! || '' });
+
 const accessToken = Cookies.get("accessToken");
-// const accessToken = localStorage.getItem('accessToken');
-axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+if (accessToken) {
+  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+}
 
 axiosInstance.interceptors.response.use(
   (response) => response.data,
