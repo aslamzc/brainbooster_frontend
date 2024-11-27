2a14985fce2a10f602bed30ce1c42e7f4c8765e8
Aslam <aslam>
2024-11-27 17:03:31 +0530

    Update

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const accessToken = request.cookies.get("accessToken")?.value;
     if (!accessToken) {
-        return NextResponse.redirect(new URL('/', request.url))
+        return NextResponse.redirect(new URL('/signin', request.url))
     }
 }
 
