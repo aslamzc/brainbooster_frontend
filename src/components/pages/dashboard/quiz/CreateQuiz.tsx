f0dd8c57756f36c22ffa60af3471282ff84c7538
Aslam <aslam>
2024-11-27 18:00:44 +0530

    Update

--- src/components/pages/dashboard/quiz/CreateQuiz.tsx
+++ src/components/pages/dashboard/quiz/CreateQuiz.tsx
@@ -1,20 +1,33 @@
 "use client";
 import { useState } from 'react';
 import axios from "@/utils/axios";
-import { Accordion, AccordionDetails, AccordionSummary, Autocomplete, Button, TextField } from '@mui/material';
+import {
+    Accordion, AccordionDetails, AccordionSummary, Autocomplete, Box, Button,
+    Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField
+} from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { form, QuizCreateFormType, Controller, questionObj, correctAnswerArr } from './QuizCreateSchema';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { useNotifications } from '@toolpad/core/useNotifications';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 
 const CreateQuiz = () => {
     const [expanded, setExpanded] = useState<number | null>(0);
     const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
     const notifications = useNotifications();
     const { handleSubmit, control, getValues, setValue, watch, reset } = form();
 
     watch('questions');
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const generateQuiz = () => {
+
+    }
 
     const onSubmit = async (data: QuizCreateFormType) => {
         try {
@@ -251,6 +264,13 @@ const CreateQuiz = () => {
                     </Grid>
                     <Button
                         variant="contained"
+                        onClick={() => { setOpen(true) }}
+                        endIcon={<AutoFixHighIcon />}
+                    >
+                        Generate Quiz
+                    </Button>
+                    <Button
+                        variant="contained"
                         onClick={() => {
                             setValue('questions', [...getValues('questions') ?? [], questionObj]);
                             setExpanded((getValues('questions')?.length ?? 0) - 1);
@@ -274,6 +294,41 @@ const CreateQuiz = () => {
                     </Grid>
                 </Grid>
             </form>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    Paragraph to Quiz Generation
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Utilizing OpenAI ChatGPT for transforming text paragraphs into engaging quizzes, enabling efficient learning and knowledge assessment.
+                    </DialogContentText>
+                    <Box sx={{ mt: 2 }}>
+                        <TextField
+                            multiline
+                            fullWidth
+                            label="Paragraph"
+                            name="paragraph"
+                            variant="outlined"
+                            minRows={10}
+                            maxRows={20}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        onClick={generateQuiz}
+                        variant="contained"
+                        color='primary'
+                    >
+                        Generate
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
