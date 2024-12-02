0e1da839c3a00501f9c5d20fbb0bc2a0eef8280e
Aslam <aslam>
2024-12-02 23:19:59 +0530

    Update

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,10 +1,25 @@
 "use client";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Dashboard() {
+
+  const [name, setName] = useState<string | null>();
+
+  useEffect(() => {
+    const getName = localStorage.getItem('userName');
+    setName(getName);
+  }, []);
   return (
     <>
-      <Typography variant="h6">Welcome {localStorage.getItem('userName')}</Typography>
+      {!!name
+        ?
+        <Typography variant="h6">Welcome {name}</Typography>
+        :
+        <Box className="flex items-start justify-center h-screen flex-col">
+          <h1 className="text-center">Loading...</h1>
+        </Box>
+      }
     </>
   );
 }
